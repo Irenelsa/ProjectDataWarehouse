f9ac7971c8abdc8eb199a456b44510bad4ec9bdf
Irene <irene.lopez.sanz@gmail.com>
2022-10-20 19:39:31 +0200

    commit dataware house project

--- etl.py
+++ etl.py
@@ -1,14 +1,26 @@
 import configparser
 import psycopg2
-from sql_queries import copy_table_queries, insert_table_queries
+from sql_queries import copy_table_queries, insert_table_queries, drop_table_queries, create_table_queries
 
+# use drop query tables from sql_queries.py file
+def drop_staging_tables(cur, conn):
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
 
+# use create query tables from sql_queries.py file
+def create_staging_tables(cur, conn):
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+# use copy query tables from sql_queries.py file to load S3 json files
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+# use insert query tables from sql_queries.py file to load analytics tables 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
@@ -16,15 +28,23 @@ def insert_tables(cur, conn):
 
 
 def main():
+    # you get file properties that has all configuracion keys to AWS and database
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # you create database conection
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
+    # you drop and create staging tables to load all S3 files
+    drop_staging_tables(cur, conn)
+    create_staging_tables(cur, conn)
     load_staging_tables(cur, conn)
+
+    # you made insert data into analytics tables from staging tables 
     insert_tables(cur, conn)
 
+    # close database connection
     conn.close()
 
 
--- sql_queries.py
+++ sql_queries.py
@@ -7,60 +7,159 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays; "
+user_table_drop = "DROP TABLE IF EXISTS users; "
+song_table_drop = "DROP TABLE IF EXISTS songs; "
+artist_table_drop = "DROP TABLE IF EXISTS artists; "
+time_table_drop = "DROP TABLE IF EXISTS time; "
+
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS staging_events (
+    artist VARCHAR(500),
+    auth VARCHAR,
+    firstName VARCHAR,
+    gender VARCHAR,
+    ItemInSession INT,
+    lastName VARCHAR,
+    length VARCHAR,
+    level VARCHAR,
+    location VARCHAR(500),
+    method VARCHAR,
+    page VARCHAR,
+    registration VARCHAR,
+    sessionId INT,
+    song VARCHAR,
+    status INT,
+    ts BIGINT,
+    userAgent VARCHAR,
+    userId INT)
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs(
+    song_id VARCHAR PRIMARY KEY NOT NULL,
+    artist_id VARCHAR,
+    artist_latitude float,
+    artist_longitude float,
+    artist_location VARCHAR(500),
+    artist_name VARCHAR(500),
+    duration float,
+    num_songs INT,
+    title VARCHAR,
+    year INT)
 """)
 
-songplay_table_create = ("""
+songplay_table_create = (""" 
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id int IDENTITY(0,1) PRIMARY KEY, 
+    start_time timestamp NOT NULL,
+    user_id int NOT NULL, 
+    level varchar, 
+    song_id varchar, 
+    artist_id varchar, 
+    session_id int, 
+    location VARCHAR(500), 
+    user_agent varchar);  
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id int PRIMARY KEY NOT NULL,
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    gender varchar NOT NULL,
+    level varchar);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id varchar PRIMARY KEY NOT NULL,
+    title varchar NOT NULL,
+    artist_id varchar NOT NULL,
+    year int NOT NULL,
+    duration float NOT NULL);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id varchar PRIMARY KEY, 
+    name VARCHAR(500) NOT NULL, 
+    location VARCHAR(500), 
+    latitude float, 
+    longitude float);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time timestamp PRIMARY KEY,
+    hour int, 
+    day int, 
+    week int, 
+    month int, 
+    year int,                      
+    weekday int);
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+    COPY staging_events
+    FROM {}
+    credentials 'aws_iam_role={}'
+    region 'us-west-2'
+    format as json {}
+""").format(config.get('S3','LOG_DATA'), config.get('IAM_ROLE','ARN'), config.get('S3','LOG_JSONPATH')) 
 
 staging_songs_copy = ("""
-""").format()
+    COPY staging_songs
+    FROM {}
+    credentials 'aws_iam_role={}'
+    region 'us-west-2'
+    json 'auto'
+    """).format(config.get('S3','SONG_DATA'), config.get('IAM_ROLE','ARN')) 
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
+INSERT INTO songplays(start_time,user_id,level,song_id,artist_id,session_id,location,user_agent)
+    SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' as start_time, e.userid, e.level, s.song_id, s.artist_id, e.sessionId, e.location, e.userAgent
+    FROM staging_events e 
+    LEFT JOIN staging_songs s ON e.song = s.title AND e.length = s.duration
+    WHERE e.page = 'NextSong'
 """)
 
-user_table_insert = ("""
-""")
+user_table_insert = """
+INSERT INTO users(user_id,first_name,last_name,gender,level)
+    SELECT userId, firstName, lastName, gender, level
+    FROM staging_events
+    where page = 'NextSong'
+"""
 
 song_table_insert = ("""
+INSERT INTO songs(song_id,title,artist_id,year,duration)
+    SELECT song_id,title, artist_id, year, duration
+    FROM staging_songs
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists(artist_id,name,location,latitude,longitude)
+    SELECT artist_id,artist_name, artist_location, artist_latitude, artist_longitude
+    FROM staging_songs
 """)
 
 time_table_insert = ("""
+INSERT INTO time(start_time,hour,day,week,month,year,weekday)
+    SELECT ts ,EXTRACT(HOUR FROM ts ) hour_ts, EXTRACT(DAY FROM ts ) day_ts, EXTRACT(WEEK FROM ts ) week_ts,
+            EXTRACT(MONTH FROM ts )month_ts, EXTRACT(YEAR FROM ts ) year_ts, EXTRACT(dayofweek FROM ts )dayofweek_ts  FROM (
+                SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' as ts
+                FROM staging_events e 
+                WHERE e.page = 'NextSong'
+            )
 """)
 
 # QUERY LISTS
@@ -68,4 +167,5 @@ time_table_insert = ("""
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [user_table_insert, song_table_insert, artist_table_insert, time_table_insert, songplay_table_insert]
+
