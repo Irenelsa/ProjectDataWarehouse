f75b35c839e6ab1159109207e222189ee3cd4b05
Irene <irene.lopez.sanz@gmail.com>
2022-10-24 10:32:33 +0200

    Include functions to create different schemas

--- sql_queries.py
+++ sql_queries.py
@@ -169,3 +169,42 @@ drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songp
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [user_table_insert, song_table_insert, artist_table_insert, time_table_insert, songplay_table_insert]
 
+
+# create separate schemas for staging, dimensions and facts and create each table in the appropriate schema.
+# CREATE SCHEMAS
+fact_schema= ("CREATE SCHEMA IF NOT EXISTS fact_tables")
+dimension_schema= ("CREATE SCHEMA IF NOT EXISTS dimension_tables")
+staging_schema= ("CREATE SCHEMA IF NOT EXISTS staging_tables")
+
+# DROP SCHEMAS
+fact_schema_drop= ("DROP SCHEMA IF EXISTS fact_tables CASCADE")
+dimension_schema_drop= ("DROP SCHEMA IF EXISTS dimension_tables CASCADE")
+staging_schema_drop= ("DROP SCHEMA IF EXISTS staging_tables CASCADE")
+
+# create_tables.py
+
+def create_schemas(cur, conn):
+    '''
+    Function to create schemas. This function uses the variable 'create_schemas_queries' defined in the 'sql_queries.py' file.
+    Parameters:
+        - curr: Cursor for a database connection
+        - conn: Database connection
+    Outputs:
+        None
+    '''
+    for query in create_schemas_queries:
+        cur.execute(query)
+        conn.commit()        
+
+def drop_schemas(cur, conn):
+    '''
+    Function to drop schemas. This function uses the variable 'drop_schemas_queries' defined in the 'sql_queries.py' file.
+    Parameters:
+        - curr: Cursor for a database connection
+        - conn: Database connection
+    Outputs:
+        None
+    '''
+    for query in drop_schemas_queries:
+        cur.execute(query)
+        conn.commit()
